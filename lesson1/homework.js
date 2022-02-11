34d1f00d4b6e7e4ed3cd38754ac2d5aa06dce104
khanh <baokhanh.gbli@gmail.com>
2022-02-11 23:22:46 +0700

    first commit

---
+++ lesson1/homework.js
@@ -0,0 +1,60 @@
+// 1
+function rectPerimeter(lenght, width) {
+  console.log(
+    "chu vi hinh chu nhat co canh",
+    lenght,
+    "x",
+    width,
+    "la: ",
+    (lenght + width) * 2
+  );
+}
+
+function rectArea(lenght, width) {
+  console.log(
+    "dien tich hinh chu nhat co canh",
+    lenght,
+    "x",
+    width,
+    "la: ",
+    lenght * width
+  );
+}
+
+//2
+function roundPerimeter(radius) {
+  console.log(
+    "chu vi hinh tron co ban kinh",
+    radius,
+    "la: ",
+    2 * 3.14 * radius
+  );
+}
+
+function roundArea(radius) {
+  console.log(
+    "dien tich hinh tron co ban kinh",
+    radius,
+    "la: ",
+    3.14 * radius * radius
+  );
+}
+
+//3
+function simpleEquation(a, b) {
+  console.log("nghiem cua phuong trinh", a, "x +", b, "la: ", -b / a);
+}
+
+//4
+function convertLength(cm) {
+  console.log(cm, "cm to mm:", cm * 10);
+  console.log(cm, "cm to m:", cm / 100);
+  console.log(cm, "cm to km:", cm / 1000);
+}
+
+rectPerimeter(2,3);
+rectArea(2,3);
+roundPerimeter(3);
+roundArea(3);
+simpleEquation(2,4);
+convertLength(100);
---
+++ lesson1/test.js
@@ -0,0 +1,14 @@
+function chuViHV(a){
+    console.log(a*4);
+}
+function dienTichHV(a){
+    console.log(a*a);
+}
+function chuViHinhTron(a){
+    console.log(2 * a*4 * 3.14);
+}
+
+
+chuViHV(4);
+dienTichHV(4);
+chuViHinhTron(4);
