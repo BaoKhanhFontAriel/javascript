f0ecd53b38072ef7c54856bf816fbfa0692564e3
khanh <baokhanh.gbli@gmail.com>
2022-02-24 17:37:18 +0700

    add folder

---
+++ baokhanhfontariel.github.io/lesson5/bmiCal/bmiCal.js
@@ -0,0 +1,18 @@
+let weight = document.getElementById("weight");
+let height = document.getElementById("height");
+let submit = document.getElementById("submit");
+if (weight != null && height != null) {
+}
+
+function calculate() {
+  let bmi = weight / height ** 2;
+
+  if (bmi >= 50) {
+
+  }
+}
+
+function showResult(){
+  const result = document.createElement("p");
+
+}
